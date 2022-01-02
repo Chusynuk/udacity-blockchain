e7265ddd67ad7befc59fc4f214a6717362fa4a3e
Eloi <recycled_loops@hotmail.com>
2022-01-02 14:43:17 +0100

    check buffer size

--- Labs-2/eth-exercice-02-Ganache/index.js
+++ Labs-2/eth-exercice-02-Ganache/index.js
@@ -5,8 +5,8 @@ const URL = 'http://127.0.0.1:7545';
 
 const web3 = new Web3(URL);
 
-const sendingAddress = '0x0103F145eAd21cE974FECEe9D93326D6eb84897a';
-const receivingAddress = '0xF7025dE2c15A14806Fe7906F4eAD528e4D7F225c';
+const sendingAddress = '0x3F5093d0181b9870b2FA307CE59889D59eF0FC43';
+const receivingAddress = '0xe43993E904Ce791ef55Ccc549fBb3A29611aBBCe';
 
 web3.eth.getBalance(sendingAddress).then(console.log);
 web3.eth.getBalance(receivingAddress).then(console.log);
@@ -23,13 +23,13 @@ const rawTransaction = {
 };
 
 // -- Step 7: Sign the transaction with the Hex value of the private key of the sender
-const privateKeySender = '9bba07a3b9d014cd023e8aae7227bc04bcd12d9b34a766fb743a55fc8676c8f3';
-const privateKeySenderHex = new Buffer(privateKeySender, 'hex');
+const privateKeySender = '3e147d5eaec8c593293fecdfd62606dc458da9d28196263a9356a0059d5c73b6';
+const privateKeySenderHex = Buffer.from(privateKeySender, 'hex');
 const Tx = new EthereumTx(rawTransaction);
-// console.log('transaction', transaction);
+console.log('privateKeySenderHex', privateKeySenderHex.length);
 Tx.sign(privateKeySenderHex)
 
-// // -- Step 8: Send the serialized signed transaction to the Ethereum network.
+// // // -- Step 8: Send the serialized signed transaction to the Ethereum network.
 const serializedTransaction = Tx.serialize();
-// console.log('serializedTransaction', serializedTransaction);
+// // console.log('serializedTransaction', serializedTransaction);
 web3.eth.sendSignedTransaction(serializedTransaction);
